62da050d0307b11bf6c83b0bd17347b9546ba5c5
Matt Gold <mattbgold@gmail.com>
2014-12-02 22:20:30 -0500

    Progress on Dilemma.js
    added new Strategy objects

--- Dilemma.js
+++ Dilemma.js
@@ -1,44 +1,57 @@
 var Dilemma;
 (function(Dilemma) {
+    var Actor = function(strategy){
+        var self = this;
+
+        //should this inherit from Strategy? it needs to be a reference
+        self.score = 0;
+        self.history = [];
+        self.strategy = strategy;
+    };
+
+    var Strategy = function(strategy) {
+        var self = this;
+
+        self.name = strategy.name || null;
+        self.algorithm = strategy.alg || null;
+    };
+
 	Dilemma.strategies=[{
         name: 'Always Defect',
-        score: 0,
         alg: function(theirHistory, myHistory) {
             return 1;
         }
     }, {
         name: 'Always Cooperate', 
-        score: 0,
         alg: function(theirHistory, myHistory) {
             return 0;
         }
     }, { 
         name: 'Tit for Tat', 
-        score: 0,
         alg: function(h) {
-            return h.length ? h[h.length-1] : 0;
+            return h.length ? h[0] : 0;
         }
     }, {
         name: 'Tat for Tit', 
-        score: 0,
         alg: function(h) {
-            return h.length ? 1-(h[h.length-1]) : 0;
+            return h.length ? 1-(h[0]) : 0;
         }
     }, { 
         name: 'Random', 
-        score: 0,
         alg: function() {
             return Math.random() >= 0.5 ? 1 : 0;
         }
     }, {
         name: 'Tit for Tat with random forgiveness',
-        score: 0,
         alg: function(h) {
             //let's forgive randomly sometimes, to prevent getting caught in a defect feedback loop
             if(Math.random() >= 0.2)
-                return h.length ? h[h.length-1] : 0;
+                return h.length ? h[0] : 0;
             else
                 return 0;
         }
     }];
+
+    Dilemma.StrategyInstance = Actor;
+    Dilemma.Strategy = Strategy;
 })(Dilemma || (Dilemma = {}));
